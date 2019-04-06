6672e849e12e78987cfbad43df0d213e345faf0d
Benard18 <benkaranja43@gmail.com>
2019-04-06 19:50:10 +0300

    Adding the modules to the admin dashboard

--- client/admin.py
+++ client/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Member)
+admin.site.register(Community)
+admin.site.register(Tournament)
+admin.site.register(Bookmarks)
+admin.site.register(Blog)
+admin.site.register(Post)
+admin.site.register(Following)
+admin.site.register(Followers)
+
