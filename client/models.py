116c2e421078c98e1b25495589c4028170629817
Benard18 <benkaranja43@gmail.com>
2019-04-06 19:50:32 +0300

    Adding the models to the system based on Member class

--- client/models.py
+++ client/models.py
@@ -1,8 +1,73 @@
 from django.db import models
 from django.core.validators import RegexValidator
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
+
+class Blog(models.Model):
+	names = models.CharField(max_length=200,default='ooof')
+
+	class Meta:
+		verbose_name_plural = 'Blogs'
+
+	def __str__(self):
+		return self.names	
+
+
+class Post(models.Model):
+	names = models.CharField(max_length=200,default='ooof')
+
+	class Meta:
+		verbose_name_plural = 'Posts'
+
+	def __str__(self):
+		return self.names	
+
+class Community(models.Model):
+	name = models.CharField(max_length=200,default='ooof')
 	
-class Members(models.Model):
+	class Meta:
+		verbose_name_plural = 'Communities'
+
+	def __str__(self):
+		return self.name	
+class Tournament(models.Model):
+	names = models.CharField(max_length=200,default='ooof')
+
+	class Meta:
+		verbose_name_plural = 'Tournaments'
+
+	def __str__(self):
+		return self.names	
+class Bookmarks(models.Model):
+	names = models.CharField(max_length=200,default='ooof')
+
+	class Meta:
+		verbose_name_plural = 'Bookmarks'
+
+	def __str__(self):
+		return self.names
+
+class Following(models.Model):
+	names = models.CharField(max_length=200,default='ooof')
+
+	class Meta:
+		verbose_name_plural = 'Followings'
+
+	def __str__(self):
+		return self.names
+class Followers(models.Model):
+	names = models.CharField(max_length=200,default='ooof')
+
+	class Meta:
+		verbose_name_plural = 'Followers'
+
+	def __str__(self):
+		return self.names
+
+class Member(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE,related_name="members")
 	username= models.CharField(max_length=20,unique=True)
 	full_name= models.CharField(max_length=20)
 	email= models.EmailField()
@@ -11,3 +76,25 @@ class Members(models.Model):
 	bio = models.CharField(max_length=300)
 	profile_pic = models.ImageField()
 	cover_pic = models.ImageField()
+	community = models.ManyToManyField(Community,  blank=True,related_name='community_member')
+	tournament = models.ManyToManyField(Tournament, blank=True,related_name='tournament_member')
+	bookmarks = models.ManyToManyField(Bookmarks, blank=True, related_name='bookmarks_member')
+	following = models.ManyToManyField(Following, blank=True, related_name='following_member')
+	followers = models.ManyToManyField(Followers, blank=True, related_name='followers_member')
+
+	def __str__(self):
+		return self.username
+
+	class Meta:
+		verbose_name_plural = "Members"	
+
+	@receiver
+	def create_member_profile(sender, instance, created, **kwargs):
+		if created:
+			Members.objects.create(user=instance)
+
+
+	@receiver
+	def save_user_profile(sender, instance, **kwargs):
+		instance.members.save()
+
