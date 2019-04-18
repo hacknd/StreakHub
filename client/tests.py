9142d00f2b0f17478f242e65b015da04d31563b0
Benard18 <benkaranja43@gmail.com>
2019-04-18 15:34:20 +0300

    The test is based on creating a superuser

--- client/tests.py
+++ client/tests.py
@@ -19,4 +19,18 @@ class AccountManagerTests(TestCase):
 		with self.assertRaises(TypeError):
 			Account.objects.create_user(email='')
 		with self.assertRaises(ValueError):
-			Account.objects.create_user(email='', password='foo')
+			Account.objects.create_user(username='',email='', password='foo')
+
+	def test_create_superuser(self):
+		admin_account = Account.objects.create_superuser(username='super',password='foo',email='')
+		self.assertEqual(admin_account.username, 'super')
+		self.assertTrue(admin_account.is_active)
+		self.assertTrue(admin_account.is_staff)
+		self.assertTrue(admin_account.is_superuser)
+		# print(type(admin_account.email))
+		self.assertEqual(len(admin_account.email), 0)
+		with self.assertRaises(ValueError):
+			Account.objects.create_superuser(
+				username='super', email='',password='foo', is_superuser=False
+				)
+
