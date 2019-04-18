80ed38a218b1c147499a6432c582f8e8a2c00ebb
Benard18 <benkaranja43@gmail.com>
2019-04-18 15:43:28 +0300

    These are the forms used in creating other staff in the system (admin i say)

---
+++ client/forms.py
@@ -0,0 +1,16 @@
+# from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+# from .models import Account
+from django.contrib.auth import get_user_model
+
+class CustomUserCreationForm(UserCreationForm):
+	class Meta(UserCreationForm):
+		model = get_user_model()
+		fields = ('username', 'email')
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+	class Meta:
+		model = get_user_model()
+		fields = ('username', 'email')		
