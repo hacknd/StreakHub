f2b5f361e898f5cf2a4c560819e226cbda182638
Benard18 <benkaranja43@gmail.com>
2019-04-18 08:47:07 +0300

    Adding the official Auth_model to the system.

--- server/settings.py
+++ server/settings.py
@@ -87,6 +87,9 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'client.Account'
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
