f6cde8c25646f7ffca0b723505f1f21b6b2c9be8
Benard18 <benkaranja43@gmail.com>
2019-04-18 15:04:59 +0300

    Adding the simple account creation so as to check the issue that would arise

--- client/tests.py
+++ client/tests.py
@@ -5,3 +5,18 @@ from django.contrib.auth import get_user_model
 Account = get_user_model()
 
 # Create your tests here.
+class AccountManagerTests(TestCase):
+	def test_create_account_test(self):
+		account = Account.objects.create_user(username='ben', email='normal@user.com', password='foo')
+		self.assertEqual(account.email, 'normal@user.com')
+		self.assertTrue(account.is_active)
+		self.assertFalse(account.is_staff)
+		self.assertFalse(account.is_superuser)
+
+
+		with self.assertRaises(TypeError):
+			Account.objects.create_user()
+		with self.assertRaises(TypeError):
+			Account.objects.create_user(email='')
+		with self.assertRaises(ValueError):
+			Account.objects.create_user(email='', password='foo')
