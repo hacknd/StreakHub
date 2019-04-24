cecf45a40ea3f122c0e322d162e1711f77b45d76
Benard18 <benkaranja43@gmail.com>
2019-04-24 12:12:01 +0300

    Removing the test urls i was using.

--- client/urls.py
+++ client/urls.py
@@ -2,8 +2,3 @@ from django.urls import path
 from client import views
 from rest_framework.authtoken.views import obtain_auth_token
 
-# urlpatterns = [
-# 	path('hello/', views.HelloView.as_view(), name='Hello' ),
-# 	path('api-token-auth/',obtain_auth_token, name='api_token_auth'),
-# 	path('example/', views.ExampleView.as_view(), name='example')
-# ]
