46af2f317b21879a5b65a17a001e4537cbcf7e6a
Benard18 <benkaranja43@gmail.com>
2019-04-24 21:01:55 +0300

    Adding the following serializers to utilize in the creation of the user registration and login

--- client/serializers.py
+++ client/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate, login
+from knox.models import AuthToken
 
-
-class AccountSerializer(serializers.ModelSerializer):
+class CreateAccountSerializer(serializers.ModelSerializer):
 	email = serializers.EmailField(
 		required=True,
 		validators=[UniqueValidator(queryset=get_user_model().objects.all())]
@@ -23,3 +23,32 @@ class AccountSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = get_user_model()
 		fields = ('id', 'username', 'email', 'password')	
+
+class AccountSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = get_user_model()
+		fields = '__all__'		
+
+class LoginUserSerializer(serializers.ModelSerializer):
+	username = serializers.CharField(
+		required=True,
+		)	
+	password = serializers.CharField(
+		min_length=8,
+		write_only=True
+		)		
+
+	def validate(self , data):
+		account = authenticate(username=data['username'], password=data['password'])
+		if account and account.is_active:
+			return account
+		raise serializers.ValidationError("Ooops! Wrong credentials, try again?")	
+
+	class Meta:
+		model = get_user_model()
+		fields = ('username', 'password')	
+
+class AuthTokenSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = AuthToken
+		fields = ('token')
