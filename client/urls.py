7dd8acab767713e6ebff5f1ffee1b790bdd98fc5
Benard18 <benkaranja43@gmail.com>
2019-04-24 12:12:17 +0300

    new API class and urls to be used

--- client/urls.py
+++ client/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from client import views
-from rest_framework.authtoken.views import obtain_auth_token
+# from rest_framework.authtoken.views import obtain_auth_token
 
+urlpatterns = [
+	path('api/users/', views.UserCreate.as_view(), name='account-create'),
+]
