894a3b9ec5b34a5581ca5d81d067ae078a6290dd
Benard18 <benkaranja43@gmail.com>
2019-04-24 12:10:06 +0300

    Adding the following modules to be used in the creation of a post method to be used in the creation of a user in the system.

--- client/views.py
+++ client/views.py
@@ -1,6 +1,12 @@
 # from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+# from rest_framework.permissions import IsAuthenticated
+# from rest_framework.authentication import SessionAuthentication , BasicAuthentication
+from rest_framework import status
+from client.serializers import *
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 
 # Create your views here.
