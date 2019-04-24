37609a662c84df4c806301694620c4bc1a018ba1
Benard18 <benkaranja43@gmail.com>
2019-04-24 20:59:57 +0300

    Adding the the register and login ssystem with the django authentication

--- client/urls.py
+++ client/urls.py
@@ -3,5 +3,6 @@ from client import views
 # from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-	path('api/users/', views.UserCreate.as_view(), name='account-create'),
+	path('auth/register/', views.AccountCreateAPI.as_view(), name='account-create'),
+	path('auth/login/', views.AccountLoginAPI.as_view(), name='account-login')
 ]
