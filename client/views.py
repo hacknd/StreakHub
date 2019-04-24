7b2ee7d98082ade73e34e3bc7d891b600b33f649
Benard18 <benkaranja43@gmail.com>
2019-04-24 12:11:08 +0300

    This is the Account Create class that I'm using in the managing of the user

--- client/views.py
+++ client/views.py
@@ -11,3 +11,19 @@ from rest_framework.authtoken.models import Token
 
 # Create your views here.
 
+class AccountCreate(APIView):
+	"""
+	Create an account from scratch
+	"""
+	def post(self, request, format='json'):
+		serializer = AccountSerializer(data=request.data)
+		if serializer.is_valid():
+			user = serializer.save()
+			if user:
+				token = Token.objects.create(user=user)
+				json = serializer.data
+				json['token'] = token.key
+				return Response(json,
+					status=status.HTTP_201_CREATED
+					)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)	
