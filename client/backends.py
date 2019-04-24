2a0da109d40312e0385cd79f9d62ab81401f55ad
Benard18 <benkaranja43@gmail.com>
2019-04-24 21:02:25 +0300

    Adding the authentication areas of the django rest framework

--- client/backends.py
+++ client/backends.py
@@ -1,24 +1,55 @@
 from django.contrib.auth import get_user_model
+# from django.conf import settings
+# from django.contrib.auth.backends import ModelBackend
 # from django.contrib.auth.models import check_password
 from django.db.models import Q
-
-
+from django.contrib.auth.models import Permission
+from rest_framework import authentication, exceptions
 Account = get_user_model()
 
 
-class AuthBackend(object):
+class AuthBackend:
 	"""
 	Authentication......starts....now
 	"""
-	def authenticate(self, username=None, password=None, **kwargs):
+	def authenticate(self, request, username=None, password=None, token=None , **kwargs):
+		if username is None:
+			username = kwargs.get(Account.USERNAME_FIELD)
 		try:
-			if email == '':
-				return None
 			# Try to fetch the account by search the username or email field
-			user = Account.objects.get(Q(username=username)|Q(email=username)|Q(phone_number=username))
-			if user.check_password(password):
-				return user
+			account = Account.objects.get(Q(username=username)|Q(email=username)|Q(phone_number=username))
+			if account.check_password(password):
+				print('oooooof')
+				return account
 		except Account.DoesNotExist:
 			# Run the default password hasher once toreduce the timing
 			# difference between an existign and a non existing existing user
-			Account.set_password(password)
+			return Account.set_password(password)
+		else:
+			if account.check_password(password) and self.user_can_authenticate(user):
+				return	account
+
+	def get_user(self, user_id):
+		try:
+			account = Account.objects.get(pk=user_id)
+		except Account.DoesNotExist:
+			return None	
+		return account if self.user_can_authenticate(account) else None 			
+
+
+class AuthApiAuthentication(authentication.BaseAuthentication):
+	def authenticate(self, request, username=None, password=None, token=None, **kwargs):
+		if username is None:
+			username = kwargs.get(Account.USERNAME_FIELD)
+		try:
+			# Try to fetch the account by search the username or email field
+			account = Account.objects.get(Q(username=username)|Q(email=username)|Q(phone_number=username))
+			if account.check_password(password):
+				print('oooooof')
+				return account
+		except Account.DoesNotExist:
+			# Run the default password hasher once toreduce the timing
+			# difference between an existign and a non existing existing user
+			raise exceptions.AuthenticationFailed('No such user')
+		
+		return (user, None)
