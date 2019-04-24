34846b2f6ae3a9e7f1265bba273485eff8ff0d28
Benard18 <benkaranja43@gmail.com>
2019-04-24 09:30:40 +0300

    Added the is_admin column because we will have two different interfaces for a normal member and an admin

--- client/models.py
+++ client/models.py
@@ -57,10 +57,10 @@ class Account(AbstractUser):
 class Member(models.Model):
 	user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE,related_name="user_members")
 	username= models.CharField(max_length=20,unique=True)
+	is_admin = models.BooleanField(default=False)
 	full_name= models.CharField(max_length=20)
 	email= models.EmailField()
-	phone_regex= RegexValidator(regex=r'^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,3})|(\(?\d{2,3}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-	phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+	phone_number = models.CharField(max_length=17, blank=True)
 	bio = models.CharField(max_length=300)
 	profile_pic = models.ImageField()
 	cover_pic = models.ImageField()
