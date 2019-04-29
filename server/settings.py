c0978f0f4d1ebdf21d812f5541cfd8f645d34f85
Benard18 <benkaranja43@gmail.com>
2019-04-29 11:20:48 +0300

    Adding the request link and middleware catered in managing the cor headers of the react app

--- server/settings.py
+++ server/settings.py
@@ -63,11 +63,18 @@ REST_FRAMEWORK = {
         )
 }
 
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000',
+    )
+
+
 AUTH_USER_MODEL = 'client.Account'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
