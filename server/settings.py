4d372ce8cc3cee378456d0beb568c718bedb6701
Benard18 <benkaranja43@gmail.com>
2019-04-29 11:20:06 +0300

    working on stagging a authentication system suitable fot the jwt token authentication

--- server/settings.py
+++ server/settings.py
@@ -47,10 +47,20 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+# REST_FRAMEWORK = {
+#     'DEFAULT': ('client.backends.AuthBackend',
+#         'rest_framework.authentication.BasicAuthentication',
+#         'knox.auth.TokenAuthentication',), 
+# }
+
 REST_FRAMEWORK = {
-    'DEFAULT': ('client.backends.AuthBackend',
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+        ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-        'knox.auth.TokenAuthentication',), 
+        )
 }
 
 AUTH_USER_MODEL = 'client.Account'
