c58ed730113f1757fa83a1fc011c12b7587f3ced
Benard18 <benkaranja43@gmail.com>
2019-04-30 09:30:28 +0300

    Disregarding the jwt format

--- client/serializers.py
+++ client/serializers.py
@@ -56,38 +56,3 @@ class AuthTokenSerializer(serializers.ModelSerializer):
 
 
 
-User = get_user_model()
-
-
-class UserSerializer(serializers.ModelSerializer):
-	
-	class Meta:
-		model = User
-		fields = ('username',)
-
-
-class UserSerializerWithToken(serializers.ModelSerializer):
-	token=serializers.SerializerMethodField()
-	password = serializers.CharField(write_only=True)
-
-	def get_token(self, obj):
-		jwt_payload_handler = __import__('rest_framework_jwt').settings.api_settings.JWT_PAYLOAD_HANDLER
-		jwt_encode_handler = __import__('rest_framework_jwt').settings.api_settings.JWT_ENCODE_HANDLER
-
-
-
-		payload = jwt_payload_handler(obj)
-		token = jwt_encode_handler(payload)
-		return token
-
-	def create(self, validated_data):
-		password = validated_data.pop('password', None)
-		instance = self.Meta.model(**validated_data)
-		if password is not None:
-			instance.set_password(password)
-		instance.save()
-		return instance
-
-	class Meta:
-		model = User
-		fields = ('token', 'username', 'password',)		
