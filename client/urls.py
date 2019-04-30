2f67c60e812a3e26cb48fc0296e7074e16e47044
Benard18 <benkaranja43@gmail.com>
2019-04-30 09:30:52 +0300

    Editing thr login application urls to suite for knox verification

--- client/urls.py
+++ client/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from client import views
-# from rest_framework.authtoken.views import obtain_auth_token
-
-# from knox import views as knox_views
-
 
 urlpatterns = [
 	path('current_user/', views.current_user),
-	path('users/', views.UserListJWT.as_view()),
 	path('auth/register/', views.AccountCreateAPI.as_view(), name='account-create'),
-	# path('auth/login/', views.AccountLoginAPI.as_view(), name='account-login'),
-	]
+	path('auth/login/', views.AccountLoginAPI.as_view(), name='account-login'),
+	path('auth/logout/',views.AccountLogoutView.as_view(), name='knox_logout'),
+	path('auth/logoutall/', views.AccountLogoutAllView.as_view(), name='knox_logoutall'),
+]
+
