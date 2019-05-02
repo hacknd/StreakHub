abf7a3b5bde24a5ad045f1a90a8c79f352dfd596
Benard18 <benkaranja43@gmail.com>
2019-05-02 17:43:49 +0300

    Added the appropriate code to send information based on the user as per the user , token and expiry date

--- client/views.py
+++ client/views.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from django.http import HttpResponseRedirect 
 from rest_framework import status, generics, mixins
 from client.serializers import *
-from django.contrib.auth import get_user_model	
+from django.contrib.auth import get_user_model,login	
+from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework import permissions, reverse
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from knox.views import LoginView
@@ -12,7 +13,7 @@ from knox.auth import TokenAuthentication
 from django.contrib.auth.signals import user_logged_out, user_logged_in
 from django.utils import timezone
 from rest_framework.decorators import api_view
-from knox.settings import knox_settings
+from knox.settings import knox_settings, CONSTANTS
 
 current_format = None
 
@@ -55,67 +56,15 @@ class AccountCreateAPI(generics.GenericAPIView):
 		return HttpResponseRedirect('/api/1.0')		
 
 
-class AccountLoginAPI(APIView):
-	authentication_classes = [BasicAuthentication,TokenAuthentication,  ]
+class AccountLoginAPI(LoginView):
+	permission_classes = (permissions.AllowAny, )
 
-	permission_classes = (permissions.IsAuthenticated,)
-
-	def get_context(self):
-		return {'request': self.request, 'format': self.format_kwarg, 'view': self}
-
-	def get_token_ttl(self):
-		return knox_settings.TOKEN_TTL
-
-	def get_token_limit_per_user(self):
-		return knox_settings.TOKEN_LIMIT_PER_USER			
-
-
-	
-
-	def post(self, request, format=None, *args, **kwargs):
-		token_limit_per_user = self.get_token_limit_per_user()
-		print(request.user)
-		if token_limit_per_user is not None:
-			now = timezone.now()
-			print(request.user)
-			token = request.user.auth_token_set.filter(expiry__gt=now)
-			if token.count() >= token_limit_per_user:
-				return Response(
-					{"error": "Maximum amount of tokens allowed per user exceeded."},
-					status=status.HTTP_403_FORBIDDEN
-					)
-		token_ttl = self.get_token_ttl()
-		instance, token = __import__('knox').models.AuthToken.objects.create(request.user, token_ttl)
-		user_logged_in.send(sender=request.user.__class__,
-							request=request, 
-							user=request.user	
-							)				
-		UserSerializer = LoginUserSerializer
-
-		data = {
-			'expiry': instance.expiry,
-			'token': token,
-			'limit': request.user.auth_token_set.filter(expiry__gt=now).count()
-		}
-
-		if UserSerializer is not None:
-			data['user'] = UserSerializer(
-				request.user,
-				context=self.get_context()
-				).data
-		return Response(data)
-
-	def get(self, request, *args, **kwargs):
-		if request.user.is_authenticated:
-			data = {
-			'ooh':'your alive',
-			'user': AccountSerializer(request.user).data
-			}
-		else:
-			data = {
-			'error': 'You saw this.'
-			}
-		return Response(data, status=status.HTTP_200_OK)
+	def post(self, request, format=None):
+		serializer=AuthTokenSerializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		user = serializer.validated_data['user']
+		login(request, user)
+		return super(AccountLoginAPI, self).post(request, format=None)
 
 
 class AccountLogoutAllView(APIView):
