432b02d79babe49f983e97f4642178b33072fc8c
Benard18 <benkaranja43@gmail.com>
2019-05-02 15:10:39 +0300

    Editing the backend authentication by adding exceptions and stuff

--- client/backends.py
+++ client/backends.py
@@ -1,11 +1,18 @@
 from django.contrib.auth import get_user_model
 from django.db.models import Q
 from django.contrib.auth.models import Permission
-from rest_framework import authentication, exceptions
+from rest_framework import authentication, exceptions, status
+from rest_framework.response import Response
+from django.utils.translation import ugettext_lazy as __
+from .utils import GamEngineException400
+from rest_framework import exceptions
+from knox.auth import TokenAuthentication
 Account = get_user_model()
 
 
-class AuthBackend(object):
+
+
+class AuthBackend:
 	"""
 	Authentication......starts....now
 	"""
@@ -13,15 +20,20 @@ class AuthBackend(object):
 		if username is None:
 			username = kwargs.get(Account.USERNAME_FIELD)
 		try:
+			# If this is an email which is blank, he or she is unable to get in with a blakn username.
+			if username == '':
+				msg=__('Not permitted to do this request.')
+				raise GamEngineException400(msg)
 			# Try to fetch the account by search the username or email field
 			account = Account.objects.get(Q(username=username)|Q(email=username)|Q(phone_number=username))
 			if account.check_password(password):
-				print('/llloooooof')
+				print('AuthBackend Authentication in play.')
 				return account
 		except Account.DoesNotExist:
-			# Run the default password hasher once toreduce the timing
+			# Run the default password hasher once to reduce the timing
 			# difference between an existign and a non existing existing user
-			return Account.set_password(self, raw_password=password)
+			return None
+
 		else:
 			if account.check_password(password) and self.user_can_authenticate(user):
 				return	account
@@ -29,7 +41,10 @@ class AuthBackend(object):
 	def get_user(self, user_id):
 		try:
 			account = Account.objects.get(pk=user_id)
+			print('AuthBackend Authentication in play.')
 		except Account.DoesNotExist:
-			return None	
+			return None
 		return account if self.user_can_authenticate(account) else None 			
 
+
+		
