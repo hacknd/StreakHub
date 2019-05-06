b1b6a3f057344431af88edb2efab28e95b239627
Benard18 <benkaranja43@gmail.com>
2019-05-06 13:08:41 +0300

    Renaming the logout and logout functions so as to seek for removing the test areas i placed with the knox authentication

--- client/urls.py
+++ client/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path, include
 from client import views
-from knox import views as knox_views
 
 
 urlpatterns = [
 	path('', views.api_root, name='root'),
 	path('auth/register/', views.AccountCreateAPI.as_view(), name='account-create'),
 	path('auth/login/', views.AccountLoginAPI.as_view(), name='account-login'),
-	path('auth/logout/',views.AccountLogoutView.as_view(), name='knox_logout'),
-	path('auth/logoutall/', views.AccountLogoutAllView.as_view(), name='knox_logoutall'),
+	path('auth/logout/',views.AccountLogoutView.as_view(), name='account-logout'),
+	path('auth/logoutall/', views.AccountLogoutAllView.as_view(), name='account-logoutall'),
 ]
 
