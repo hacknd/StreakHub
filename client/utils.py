afa5fed78106b6b86adfdcd1b20f932f559d7b61
Benard18 <benkaranja43@gmail.com>
2019-05-07 08:39:18 +0300

    Adding some exceptions based on the system as per the application and learning on exceptions

--- client/utils.py
+++ client/utils.py
@@ -4,3 +4,14 @@ class GamEngineException400(exceptions.APIException):
 	status_code = 400
 	default_detail = 'This is a No No'
 	default_code = 'service_unavailable'
+
+class GamEngineException404(exceptions.APIException):
+	status_code = 404
+	default_detail="We couldn't find your request"
+	default_code="Error in Viewing"
+
+class GamEngineException500(exceptions.APIException):
+	status_code = 500
+	default_detail="Error in system functionality"
+	default_code="Error in System"	
+
