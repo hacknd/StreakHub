07e0c7111c23b181c4dd59377c0e576f65a15f6d
Benard18 <benkaranja43@gmail.com>
2019-05-15 16:12:31 +0300

    Adding the various pipelines based on the authentication of a user who uses the social route

--- server/settings.py
+++ server/settings.py
@@ -150,6 +150,17 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+SOCIAL_AUTH_PIPELINE=(
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    )
 AUTHENTICATION_BACKENDS = (
     'client.backends.AuthBackend',
     'django.contrib.auth.backends.ModelBackend',
