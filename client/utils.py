57f817d5cee84c344d0d1587e5fddd3c4a7d09e6
Benard18 <benkaranja43@gmail.com>
2019-05-16 10:34:32 +0300

    Adding an authentication backend so as to enable the redirect and creation a user instance. Based on the social_core and social_django methods and redirect features

--- client/utils.py
+++ client/utils.py
@@ -15,3 +15,30 @@ class GamEngineException500(exceptions.APIException):
 	default_detail="Error in system functionality"
 	default_code="Error in System"	
 
+def GamEngineRedirectAuthorizationBackend(backend, code):
+		"""
+		Authorization information for the backends to the redirect feature
+		"""
+		if code == None:
+			try:
+				backend_class=get_backend(AUTHENTICATION_BACKENDS, backend)
+				authorization_url=backend_class.AUTHORIZATION_URL
+				url_parameters={
+					'redirect_uri':('http://{}{}').format(Site.objects.get_current().domain, reverse('account-social-login', args=(backend,))),
+					'response_type':backend_class.RESPONSE_TYPE,
+					'scope':[scope for scope in backend_class.DEFAULT_SCOPE if scope != 'openid'][0],
+					'client_id':config('SOCIAL_AUTH_'+backend.upper().replace('-','_')+'_KEY')
+				}
+				final_url=authorization_url+'?'+__import__('urllib').parse.urlencode(url_parameters)
+				print(final_url)
+			except:
+				raise GamEngineException(code=status.HTTP_501_NOT_IMPLEMENTED,detail=__('Missing Backend'))
+			
+			return redirect(final_url)
+		
+		data = {
+			"code":code,
+			"redirect_uri":('http://{}{}').format(Site.objects.get_current().domain, reverse('account-social-login', args=(backend,))),
+			"provider":backend
+		}
+		return	data
