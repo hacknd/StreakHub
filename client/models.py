4ed3aa6d21c931a1c8e0b03291da56d69be54fa4
Benard18 <benkaranja43@gmail.com>
2019-05-19 11:36:12 +0300

    Moving the account and role class from client to client auth app

--- client/models.py
+++ client/models.py
@@ -1,66 +1,8 @@
 from django.db import models
-from django.core.validators import RegexValidator
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
-from django.utils.translation import ugettext_lazy as __
-from .managers import CustomAccountManager
-
-class Role(models.Model):
-	'''
-	The Role Entries are managed in the system to decide who is who in the system
-
-	'''
-	ACCOUNT = 1
-	INDIVIDUAL_ACCOUNT = 2
-	TOURNAMENT_ADMIN = 3
-	TOURNAMENT_SUPERVISOR = 4
-	TOURNAMENT_FS = 5
-	ADVERT_ACCOUNT = 6
-	VERIFIED_ACCOUNT = 7
-
-	ROLE_CHOICES = (
-		(ACCOUNT, 'member'),
-		(INDIVIDUAL_ACCOUNT, 'individual_account'),
-		(TOURNAMENT_ADMIN, 'tournament_admin'),
-		(TOURNAMENT_SUPERVISOR, 'tournament_supervisor'),
-		(TOURNAMENT_FS, 'tournament_field_scorer'),
-		(ADVERT_ACCOUNT, 'advertisement_account'),
-		(VERIFIED_ACCOUNT, 'verified_account')
-	)
-	id = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, primary_key=True, default=ACCOUNT)
-
-	def save_role(self):
-		self.save()
-
-	def role_count(self):
-		return len(self.ROLE_CHOICES)	
-	def __str__(self):
-		return self.get_id_display()
-
-class Account(AbstractUser):
-	username = models.CharField(max_length=13, unique=True)
-	email = models.EmailField(__('email address'))
-	is_email_active = models.BooleanField(default=False)
-	phone_regex= RegexValidator(regex=r'^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,3})|(\(?\d{2,3}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-	phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
-	is_phone_active = models.BooleanField(default=False) # Activating the phone number to be of use.
-	account_membership_id = models.CharField(max_length=10, unique=True)
-
-
-	USERNAME_FIELD = 'username'
-	REQUIRED_FIELDS = []
-
-	objects = CustomAccountManager()
-
-	@receiver(post_save, sender='client.Member')
-	def account_membership_id(sender, instance, created, **kwargs):
-		if created:
-			pass
-
-	def __str__(self):
-		return self.username
+from client_auth.models import Role
 
 class Member(models.Model):
 	user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE,related_name="user_members")
@@ -72,12 +14,12 @@ class Member(models.Model):
 	bio = models.CharField(max_length=300)
 	profile_pic = models.ImageField()
 	cover_pic = models.ImageField()
-	community = models.ManyToManyField('client.community',  blank=True,related_name='community_member')
-	tournament = models.ManyToManyField('client.tournament', blank=True,related_name='tournament_member')
-	bookmarks = models.ManyToManyField('client.bookmarks', blank=True, related_name='bookmarks_member')
-	following = models.ManyToManyField('client.following', blank=True, related_name='following_member')
-	followers = models.ManyToManyField('client.followers', blank=True, related_name='followers_member')
-	roles = models.ManyToManyField('client.role',related_name='user_role')
+	community = models.ManyToManyField('client.Community',  blank=True,related_name='community_member')
+	tournament = models.ManyToManyField('client.Tournament', blank=True,related_name='tournament_member')
+	bookmarks = models.ManyToManyField('client.Bookmarks', blank=True, related_name='bookmarks_member')
+	following = models.ManyToManyField('client.Following', blank=True, related_name='following_member')
+	followers = models.ManyToManyField('client.Followers', blank=True, related_name='followers_member')
+	roles = models.ManyToManyField('client_auth.Role',related_name='user_role')
 
 
 	def initialize_default_role(user):
@@ -115,15 +57,6 @@ class Member(models.Model):
 		
 	def __str__(self):
 		return self.user.username 
-
-class RegistrationAudit(models.Model):
-	user_id=models.CharField(max_length=300,unique=True, default='1')
-	session_id = models.CharField(max_length=300, unique=True, default='1')
-	pass
-
-	
-	class Meta:
-		verbose_name_plural = 'RegistrationAudits'
 
 
 class Blog(models.Model):
