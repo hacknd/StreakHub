9ff43c9e93655ade86d9e02f7caada990aa9c8b9
Benard18 <benkaranja43@gmail.com>
2019-05-19 11:49:12 +0300

    whitespace

--- client_auth/serializers.py
+++ client_auth/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from rest_social_auth.serializers import UserKnoxSerializer
 from django.contrib.auth import get_user_model, authenticate, login
 from knox.models import AuthToken
+
 class CreateAccountSerializer(serializers.ModelSerializer):
 	email = serializers.EmailField(
 		required=True,
