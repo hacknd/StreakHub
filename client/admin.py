ecda2e27907710ac6b46adab532b3ec1e17f4936
Benard18 <benkaranja43@gmail.com>
2019-05-19 11:34:15 +0300

    Moving the admin edit methods from client.admin to client_auth.admin

--- client/admin.py
+++ client/admin.py
@@ -1,31 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
 from .models import *
 
-from .forms import *
-
-
-class CustomUserAdmin(UserAdmin):
-	add_form = CustomUserCreationForm
-	form = CustomUserChangeForm
-	model = get_user_model()
-	list_display = ('username','email','is_staff', 'is_active',)
-	list_filter =('email','is_staff', 'is_active',)
-	fieldsets = (
-		(None, {'fields':('username', 'email', 'password')}),
-		('Permissions', {'fields':('is_staff', 'is_active')}),
-		)
-	add_fieldsets = (
-		(None, {
-			'classes':('wide',),
-			'fields': ('username','email','password1','password2','is_staff','is_active')
-			}),
-		)
-	search_fields = ('username','email',)
-	ordering = ('username','email',)
-
-admin.site.register(get_user_model(), CustomUserAdmin)	
 admin.site.register(Member)
 admin.site.register(Community)
 admin.site.register(Tournament)
