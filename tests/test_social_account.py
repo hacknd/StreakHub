e1580f9590adea2945ce7e3389884fe1b773047f
Benard18 <benkaranja43@gmail.com>
2019-05-29 11:34:38 +0300

    domain information mock json dump (dump_json #1)

--- tests/test_social_account.py
+++ tests/test_social_account.py
@@ -15,3 +15,11 @@ class AccountSocialAccountTest(APITestCase):
 		Setting up the new site for the new default application
 		'''
 		httpretty.enable()
+	def _domain_information_pull_with_json(self, provider):
+		return json.dumps(
+			{
+			"code":"oofgawd",
+			"redirect_uri":('http://{}{}').format(Site.objects.get_current().domain, reverse.reverse('account-social-login', args=(provider,))),
+			"provider":provider
+			}
+			)	
