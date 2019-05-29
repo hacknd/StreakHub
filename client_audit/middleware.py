9923d054c4f344d8d6598cb08c18093d301c7333
Benard18 <benkaranja43@gmail.com>
2019-05-29 14:01:46 +0300

    defining regex sign to code

--- client_audit/middleware.py
+++ client_audit/middleware.py
@@ -9,7 +9,7 @@ def audit_middleware(get_response):
 
     def store_action(request, blocked):
         namespace = resolve(request.path).route
-        new_body=(__import__('re').sub('password=[^\/]+&', '', (request.body).decode('utf-8'))).encode('utf-8') 
+        new_body=(__import__('re').sub(r'password=[^\/]+&', '', (request.body).decode('utf-8'))).encode('utf-8') 
         UserActions.objects.create(
             route=namespace,
             method=request.method,
