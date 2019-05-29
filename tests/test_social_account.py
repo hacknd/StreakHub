523d159339e4912f9256b44b25c8eff914b9c428
Benard18 <benkaranja43@gmail.com>
2019-05-29 11:34:04 +0300

    Set up social_test_case (test #2)

--- tests/test_social_account.py
+++ tests/test_social_account.py
@@ -1,1 +1,17 @@
 from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
+from rest_framework import test, reverse
+from django.utils.translation import ugettext_lazy as __
+import httpretty
+from django.contrib.sites.models import Site
+import json
+import requests
+
+
+
+class AccountSocialAccountTest(APITestCase):
+	def setUp(self):
+		'''
+		Setting up the new site for the new default application
+		'''
+		httpretty.enable()
