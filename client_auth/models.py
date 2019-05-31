b520181ce151422735fa59f805d00b1fc8daff77
Benard18 <benkaranja43@gmail.com>
2019-05-31 08:54:33 +0300

    setting to pep8 standards

--- client_auth/models.py
+++ client_auth/models.py
@@ -13,10 +13,21 @@ class Account(AbstractUser):
 	username = models.CharField(max_length=13, unique=True)
 	email = models.EmailField(__('email address'))
 	is_email_active = models.BooleanField(default=False)
-	phone_regex = RegexValidator(regex=r'^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,3})|(\(?\d{2,3}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-	phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+	phone_regex = RegexValidator(
+		regex=r'^((\+\d{1,3}(-| )?\(?\d\)?(-| )?\d{1,3})|(\(?\d{2,3}\)?))(-| )?(\d{3,4})(-| )?(\d{4})(( x| ext)\d{1,5}){0,1}$',
+		message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."
+		)
+	phone_number = models.CharField(
+		validators=[phone_regex],
+		max_length=17,
+		blank=True
+		)
 	is_phone_active = models.BooleanField(default=False)
-	account_membership_id = models.CharField(max_length=20, unique=True, blank=True)
+	account_membership_id = models.CharField(
+		max_length=20,
+		unique=True,
+		blank=True
+		)
 
 	USERNAME_FIELD = 'username'
 	REQUIRED_FIELDS = []
