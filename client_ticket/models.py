d7021002c741e893bf9aedfd2572eec7dfc7670d
Benard18 <benkaranja43@gmail.com>
2019-06-03 09:17:19 +0300

    Ticket Number model

--- client_ticket/models.py
+++ client_ticket/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+from .managers import AccountTicketNumberManager
 # Create your models here.
+class AccountTicketNumber(models.Model):
+	objects = AccountTicketNumberManager()
+	pass
