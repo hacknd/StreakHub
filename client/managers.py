2176d745c8a4428419a1f98a8eace062f1d14eff
Benard18 <benkaranja43@gmail.com>
2019-06-08 23:16:03 +0300

    Remove follower manager added

--- client/managers.py
+++ client/managers.py
@@ -1,6 +1,7 @@
 from django.db import models
 from client.urls import urlpatterns
 from client_auth.utils import GamEngineException
+from django.utils.translation import ugettext_lazy as __
 
 class FollowManager(models.Manager):	
 	def followers(self, user):
@@ -20,4 +21,10 @@ class FollowManager(models.Manager):
 		return relation
 
 	def remove_follower(self, followers, followee):
-		return 'string'
+		try:
+			qs = self.model.objects.get(followers=followers, followee=followee)
+			qs.delete()
+			return True
+		except self.model.DoesNotExist:
+			raise GamEngineException(code=301, detail=__('No Follow Relation'))
+				
